06083883154edb09746cf1c3902d7c15c6617dfc
DenPolosin <den.polosin@mail.ru>
2016-07-15 12:38:00 +0300

    create fix

--- retailcrm/bundle/sql/categories.sql
+++ retailcrm/bundle/sql/categories.sql
@@ -1,8 +1,9 @@
 SELECT
   `categories`.`category_id` AS `id`,
-  `name` AS `path`,
+  `seo_names`.`name` AS `url`,
   `parent_id` AS `parentId`,
   `category` AS `name`
 FROM `categories`
   INNER JOIN `category_descriptions` ON `category_descriptions`.`category_id` = `categories`.`category_id`
   INNER JOIN `seo_names` ON `seo_names`.`object_id` = `categories`.`category_id`
+WHERE `type` = 'c';
